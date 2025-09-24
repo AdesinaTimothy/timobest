ff8925ea340f96e481f3f35f62a841368b024319
Your Full Name <timobest121@gmail.com>
2025-09-24 16:06:16 +0100

    fhfh

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,13 +1,14 @@
 import { Separator } from "@/components/ui/separator";
 import { Linkedin, Instagram, Mail, TwitterIcon } from "lucide-react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className="flex flex-col gap-5 w-full h-auto">
-      <h4 className="text-yellow-500 font-semibold text-3xl">
+      <h4 className="text-yellow-500 font-semibold text-3xl text-center md:text-start">
         Let's Connect here
       </h4>
-      <div className="flex flex-wrap">
+      <div className="flex items-center justify-center md:justify-start">
         <div className="flex gap-4">
           <a
             href="https://www.linkedin.com/in/timothy-adesina-bb1188221/"
@@ -23,16 +24,24 @@ const Footer = () => {
           >
             <Instagram className="w-6 h-6 text-yellow-500 hover:text-pink-500" />
           </a>
-          <a href="">
+          <a href="mailto:timobest121@gmail.com">
             <Mail className="w-6 h-6 text-yellow-500 hover:text-yellow-500" />
           </a>
           <a href="https://x.com/timobest121" target="_blank" rel="noreferrer">
-            <TwitterIcon className="w-6 h-6 text-yellow-500 hover:text-blue-400" />
+            <div>
+              <Image
+                src="/images/icons/svg/twitter.png"
+                alt="Picture of the author"
+                width={22}
+                height={22}
+                className="bg-yellow-500 rounded-lg"
+              />
+            </div>
           </a>
         </div>
       </div>
 
-      <div className="max-w-6xl mx-auto px-6">
+      <div className="max-w-6xl mx-auto mt-4 px-6">
         <div className="text-center space-y-6">
           <div className="inline-flex items-center justify-center space-x-3 bg-black/50 backdrop-blur-sm px-8 py-4 rounded-full border border-yellow-400/20">
             <span className="text-gray-200">Beautifully crafted with</span>
